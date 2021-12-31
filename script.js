720c20aa57c0abf44f45e6bd8cf0f71d3dd19a94
its-debashis <debahalder9914@gmail.com>
2021-12-31 23:47:42 +0530

    small changes

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let chars, particles, canvas, ctx, w, h, current;
 let duration = 5000;
-let str = ['From DEBA','TO','All Star','HAPPY', 'NEW', 'YEAR' , '2022','TO','TUSHAR','BIJOY','SAGAR','GULLA','ANU','ANKIT','SUDIPTO','RIJU','CHATUA','KARAN','SURAJ','SUVO','SUDIPTO','CHAYAN','HIMANGSU','TAPAS'];
+let str = ['From DEBA','TO','MEMBER OF All Star','HAPPY', 'NEW', 'YEAR' , '2022','TO','TUSHAR','BIJOY','SAGAR','GULLA','ANU','ANKIT','SUDIPTO','RIJU','CHATUA','KARAN','SURAJ','SUVO','SUDIPTO','CHAYAN','HIMANGSU','TAPAS','ALL OF YOU'];
 
 init();
 resize();
